1a07ff560c2a6901ef05d63b2132050d3660d292
maxey <emgireyes17@gmail.com>
2026-09-18 22:06:18 +0800

    wrap router in tanstack query provider

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router";
+import AppRoutes from "./routes";
+
+const queryClient = new QueryClient();
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+}
