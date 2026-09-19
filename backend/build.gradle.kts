7a041c8e2ebd1a2f2742d4de19354d4d3b192f12
maxey <emgireyes17@gmail.com>
2026-09-19 04:08:45 +0000

    drop stale todo comment and add trailing newline to backend build file

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("org.springframework.boot") version "4.1.1"
-    id("io.spring.dependency-management") version "1.1.7" // verify latest
+    id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version "2.4.20"
     kotlin("plugin.spring") version "2.4.20"
     kotlin("plugin.jpa") version "2.4.20"
