c0785c8d139ce24f8fd869522f49d2a3ed8eef04
maxey <emgireyes17@gmail.com>
2026-09-19 16:14:35 +0800

    define a Spring repository that automatically provides standard CRUD to fetch users by email

---
+++ backend/src/main/kotlin/ph/anevaino/user/UserRepository.kt
@@ -0,0 +1,8 @@
+package ph.anevaino.user
+
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface UserRepository : JpaRepository<User, UUID> {
+    fun findByEmail(email: String): User?
+}
