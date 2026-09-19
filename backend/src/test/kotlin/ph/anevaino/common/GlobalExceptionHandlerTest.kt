e3ef6a797a577b1dceaf07a1445f97640a5329ba
maxey <emgireyes17@gmail.com>
2026-09-19 12:54:12 +0800

    add missing import lines

--- backend/src/test/kotlin/ph/anevaino/common/GlobalExceptionHandlerTest.kt
+++ backend/src/test/kotlin/ph/anevaino/common/GlobalExceptionHandlerTest.kt
@@ -3,7 +3,7 @@ package ph.anevaino.common
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest
 import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
